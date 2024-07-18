5812da395d031f9bf1a82b980f197aec1e5728c7
Immi01 <immanuel.ehe@gmx.at>
2024-07-18 12:03:44 +0200

    created first query

--- dbAccess/people.php
+++ dbAccess/people.php
@@ -7,11 +7,25 @@
 </head>
 <body>
     <a href="../index.php">Home</a>
+    <br>
+    <br>
+
     <?php
     include  './vendor/autoload.php';
 
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
     $dotenv->load();
+
+    $mysqli = mysqli_connect($_SERVER["MARIADB_IP"], $_SERVER["MARIADB_USER"], $_SERVER["MARIADB_PASS"], $_SERVER["MARIADB_DB"]);
+    
+    $results = mysqli_query($mysqli, "SELECT * FROM people;");
+    
+    echo var_dump(get_object_vars($results)), "<br><br>";
+
+    while($row = $results->fetch_assoc()) {
+        echo $row["id"] . " " . $row["name"] . " " . $row["lastname"] . "<br>";
+    }
+
     ?>
 </body>
 </html>
