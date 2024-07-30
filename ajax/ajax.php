f8857f04dfb433a785a5f2eecd4b69a320eb4d18
Immi01 <immanuel.ehe@gmx.at>
2024-07-30 18:20:27 +0200

    added another ajax example

--- ajax/ajax.php
+++ ajax/ajax.php
@@ -5,15 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="script.js" defer></script>
     <title>Document</title>
+    <style>
+        table {
+            border: 1px solid black;
+        }
+    </style>
 </head>
 <body>
     <a href="../index.php">Home</a>
     <br>
     <br>
-    <div id="demo">
+    <label for="changeText" id="demo">
         Change this text
-    </div>
-    <input type="button" value="do it" onclick="changeText()">
+    </label>
+    <input type="button" value="do it" name="changeText" onclick="changeText()">
+
+    <br>
+    <br>
+    <label for="getPeople">Get all People</label>
+    <input type="button" value="Get People" name="getPeople" onclick="getPeople()">
+    <br>
+    <br>
+    <div id="people"></div>
 
 </body>
 </html>
--- ajax/script.js
+++ ajax/script.js
@@ -3,13 +3,28 @@ function changeText() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = () => {
         if (xmlhttp.readyState == xmlhttp.DONE && xmlhttp.status == 200) {
-            console.log(xmlhttp.response);
-            console.log(xmlhttp.responseText);
             document.getElementById("demo").innerHTML = xmlhttp.responseText;
         }
     };
 
-    xmlhttp.open("GET", "testData.php", true);
+    xmlhttp.open("GET", "changeText.php", true);
     xmlhttp.send();
     
+}
+
+function getPeople() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState == xhr.DONE && xhr.status == 200) {
+            var people = JSON.parse(xhr.response);
+            var html = "<table><tr><th>Id</th><th>Vorname</th><th>Nachname</th></tr>";
+            people.forEach(person => {
+                html += `<tr><td>${person.id}</td><td>${person.firstName}</td><td>${person.lastName}</td></tr>`;
+            });
+            document.getElementById("people").innerHTML = html;
+        }
+    };
+    xhr.open("GET", "getPeople.php", true);
+    xhr.send();
+
 }
